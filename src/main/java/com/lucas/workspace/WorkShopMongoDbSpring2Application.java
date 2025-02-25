fd4e1af51c3d7311c8253f7578db9e4688944aee
Lucas Monteiro <lucasggtyy358@gmail.com>
2025-02-25 08:48:51 -0300

    First commit

---
+++ src/main/java/com/lucas/workspace/WorkShopMongoDbSpring2Application.java
@@ -0,0 +1,13 @@
+package com.lucas.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoDbSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopMongoDbSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/workspace/WorkShopMongoDbSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopMongoDbSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
